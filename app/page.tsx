ac2a64131c6cbb19d2124d72dff57c60b0b9a768
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-12-02 15:36:18 +0530

    created & designed the SearchBar using headlessui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
+import CustomFilters from '@/components/CustomFilters';
 import Hero from '@/components/Hero';
+import SearchBar from '@/components/SearchBar';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore The Car You Might Like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilters title="fuel" />
+
+            <CustomFilters title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
